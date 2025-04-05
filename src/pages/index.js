809b264fa44328e44590681e1d551b73c728bebb
vishal <vkumars426675@gmail.com>
2025-04-05 19:29:32 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from "../components/Header"; 
+import Header from "../components/header"; 
 import HeroSection from '../components/HeroSection';
 import Service from '../components/Service';
 import Clinic from '../components/Clinic';
@@ -11,7 +11,7 @@ import Footer from '../components/Footer';
 function Index() { 
   return (
     <div>
-      <Header />
+      <Header/>
       <HeroSection/>
       <Service/>
       <Clinic/>
