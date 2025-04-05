8c7f35e8ecaa12447f3dcc74e3bf8cc128a3d821
vishal <vkumars426675@gmail.com>
2025-04-05 20:20:49 +0530

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,47 +5,47 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-// import * as React from "react"
-// import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-// import Header from "./header"
-// import "./layout.css"
+import Header from "./header"
+import "./layout.css"
 
-// const Layout = ({ children }) => {
-//   const data = useStaticQuery(graphql`
-//     query SiteTitleQuery {
-//       site {
-//         siteMetadata {
-//           title
-//         }
-//       }
-//     }
-//   `)
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
-//   return (
-//     <>
-//       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-//       <div
-//         style={{
-//           margin: `0 auto`,
-//           maxWidth: `var(--size-content)`,
-//           padding: `var(--size-gutter)`,
-//         }}
-//       >
-//         <main>{children}</main>
-//         <footer
-//           style={{
-//             marginTop: `var(--space-5)`,
-//             fontSize: `var(--font-sm)`,
-//           }}
-//         >
-//           © {new Date().getFullYear()} &middot; Built with
-//           {` `}
-//           <a href="https://www.gatsbyjs.com">Gatsby</a>
-//         </footer>
-//       </div>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `var(--space-5)`,
+            fontSize: `var(--font-sm)`,
+          }}
+        >
+          © {new Date().getFullYear()} &middot; Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
 
-// export default Layout
+export default Layout
